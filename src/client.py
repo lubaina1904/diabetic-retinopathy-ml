b3cb09e23acadce2146f661e577f9962db5f6aec
lubaina1904 <lubaina.h2024@vitstudent.ac.in>
2025-12-02 12:41:06 +0530

    Simplify docstrings
    Removed extensive docstrings for clarity and brevity.

--- src/client.py
+++ src/client.py
@@ -1,10 +1,4 @@
-"""
-FEDERATED LEARNING CLIENT - Hospital Side
 
-This represents ONE hospital in the federated learning system.
-
-Key concept: Train locally, share only model updates!
-"""
 
 import flwr as fl
 import torch
@@ -15,15 +9,7 @@ import numpy as np
 
 
 class HospitalClient(fl.client.NumPyClient):
-    """
-    Federated Learning Client using FedAvg algorithm
-
-    This is what runs at each hospital:
-    1. Receives global model from server
-    2. Trains on local data
-    3. Sends updated weights back
-    """
-
+    
     def __init__(
         self,
         model: nn.Module,
@@ -33,17 +19,7 @@ class HospitalClient(fl.client.NumPyClient):
         learning_rate: float = 0.001,
         local_epochs: int = 3
     ):
-        """
-        Initialize client
-
-        Args:
-            model: Neural network
-            train_loader: This hospital's training data
-            val_loader: This hospital's validation data
-            device: 'cuda' or 'cpu'
-            learning_rate: Learning rate for local training
-            local_epochs: How many epochs to train per FL round
-        """
+        
         self.model = model.to(device)
         self.train_loader = train_loader
         self.val_loader = val_loader
@@ -62,25 +38,11 @@ class HospitalClient(fl.client.NumPyClient):
         print("  Local epochs per round: {}".format(local_epochs))
 
     def get_parameters(self, config: Dict) -> List[np.ndarray]:
-        """
-        Extract model parameters as numpy arrays
-
-        Called by server to get current model weights
-
-        Returns:
-            List of model weights as numpy arrays
-        """
+        
         return [val.cpu().numpy() for val in self.model.state_dict().values()]
 
     def set_parameters(self, parameters: List[np.ndarray]) -> None:
-        """
-        Load parameters into model
-
-        Called by server to send global model to this hospital
-
-        Args:
-            parameters: List of model weights as numpy arrays
-        """
+        
         params_dict = zip(self.model.state_dict().keys(), parameters)
         state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
         self.model.load_state_dict(state_dict, strict=True)
@@ -90,24 +52,7 @@ class HospitalClient(fl.client.NumPyClient):
         parameters: List[np.ndarray],
         config: Dict
     ) -> Tuple[List[np.ndarray], int, Dict]:
-        """
-        Train model on local data
-
-        This is the CORE of federated learning!
-
-        Steps:
-        1. Receive global model
-        2. Train on local hospital data
-        3. Return updated weights
-
-        Args:
-            parameters: Global model weights from server
-            config: Configuration dictionary
-
-        Returns:
-            tuple: (Updated model parameters, Number of samples, Metrics dict)
-        """
-        # 1. Set global parameters
+        
         self.set_parameters(parameters)
 
         # 2. Train locally
@@ -129,22 +74,9 @@ class HospitalClient(fl.client.NumPyClient):
         parameters: List[np.ndarray],
         config: Dict
     ) -> Tuple[float, int, Dict]:
-        """
-        Evaluate global model on local data
-
-        Tests how well the global model works for this hospital
-
-        Args:
-            parameters: Global model weights
-            config: Configuration dictionary
-
-        Returns:
-            tuple: (Loss value, Number of samples, Metrics dict)
-        """
-        # Set global parameters
+       
         self.set_parameters(parameters)
 
-        # Evaluate
         loss, accuracy = self._evaluate_local()
 
         return (
@@ -154,14 +86,7 @@ class HospitalClient(fl.client.NumPyClient):
         )
 
     def _train_local(self) -> Tuple[float, float]:
-        """
-        Local training loop
-
-        This happens INSIDE the hospital - data doesn't leave!
-
-        Returns:
-            tuple: (Average loss, Average accuracy)
-        """
+        
         self.model.train()
         total_loss = 0.0
         correct = 0
@@ -193,12 +118,7 @@ class HospitalClient(fl.client.NumPyClient):
         return avg_loss, accuracy
 
     def _evaluate_local(self) -> Tuple[float, float]:
-        """
-        Local evaluation
-
-        Returns:
-            tuple: (Average loss, Average accuracy)
-        """
+    
         self.model.eval()
         total_loss = 0.0
         correct = 0
